5ace8a1d81067449b55907bde19a66f0a07f23aa
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-05-10 06:53:40 +0530

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import Transactions from "./pages/Transaction";
 import Navbar from "./components/Navbar";
 import Income from "./pages/Income";
 import Expense from "./pages/Expense";
@@ -27,7 +26,6 @@ function Layout() {
         <Route path="/dashboard" element={<Dashboard />} />
          <Route path="/income" element={<Income />} />
           <Route path="/expense" element={<Expense />} />
-        <Route path="/transactions" element={<Transactions />} />
         <Route path="/reports" element={<Reports />} />
         <Route path="/categories" element={<Categories />} />
         <Route></Route>
