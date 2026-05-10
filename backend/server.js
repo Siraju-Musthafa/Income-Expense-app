b4d796cc44ce4c56fadb8feec099c0c73ac62e87
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-05-10 06:30:38 +0530

    Updated API URL for production

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 app.use("/api/", transactionRoutes);
 app.use("/api/dashboard", dashboardRoutes);
